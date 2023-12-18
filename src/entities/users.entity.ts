d26931247441014bdfd89b55ea2fde03590f3e40
Nicolas <109254154+kZNick@users.noreply.github.com>
2023-12-18 00:31:27 -0300

    fix: migrations user

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -51,10 +51,10 @@ class User {
     }
   }
 
-  @OneToMany(() => Post, (post) => post.user)
+  @OneToMany(() => Post, (post) => post.user,{cascade: true})
   posts: Post[];
 
-  @OneToMany(() => Comment, comment => comment.user)
+  @OneToMany(() => Comment, comment => comment.user,{cascade: true})
   comments: Comment[];  
 }
 
